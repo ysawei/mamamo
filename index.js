3bfa5f9dee7ba10f8c9bc421ede5b774ec4e3167
colystutorial <159697021+colystutorial@users.noreply.github.com>
2024-02-11 20:14:11 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,97 @@
+/**
+  GIT : https://github.com/colystutorial/clybgrmvr
+  DISCORD SERVER : https://discord.gg/myxlrie
+ * **********************************************
+ *   Code by COLYFLOUR
+ * **********************************************
+ */
+
+
+
+const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to COLY: http://localhost:${port}`);
+  console.log(`🔗 Powered By COLY`);
+});
+
+
+const statusMessages = ["PUT STATUS 1","PUT STATUS 2","PUT STATUS 3"];
+
+
+let currentIndex = 0;
+const channelId = '';
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+/**
+  GIT : https://github.com/colystutorial/clybgrmvr
+  DISCORD SERVER : https://discord.gg/myxlrie
+ * **********************************************
+ *   Code by COLYFLOUR
+ * **********************************************
+ */
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'dnd',
+  });
+
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+   
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
+client.once('ready', () => {
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
+});
+
+login();
+
+/**
+  GIT : https://github.com/colystutorial/clybgrmvr
+  DISCORD SERVER : https://discord.gg/myxlrie
+ * **********************************************
+ *   Code by COLYFLOUR
+ * **********************************************
+ */
